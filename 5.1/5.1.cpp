93ec72154968b2b409b0f297e135b0b18297097f
Ysntska <yasynetska27@gmail.com>
2020-11-09 09:34:53 +0200

    first

---
+++ 5.1/5.1.cpp
@@ -0,0 +1,18 @@
+﻿// Lab 5_1
+#include <iostream>
+#include <cmath>
+using namespace std;
+double h(const double x, const double y, const double z); // прототип
+int main()
+{
+	double a, b;
+    cout << "a = "; cin >> a;
+	cout << "b = "; cin >> b;
+	double c = (h(a * a, 1., b) + h(b, 1., a * a));
+	cout << "c = " << c << endl;
+	return 0;
+}
+double h(const double x, const double y, const double z) // визначення
+{
+	return (x + y + z) / (x * x + y * y);
+}
